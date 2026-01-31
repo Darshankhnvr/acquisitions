142110607f1398cc3f02ceb0112e7786632f2380
Darshan K <darshankgoudahnvr@gmail.com>
2026-01-31 16:09:51 +0530

    prettier setup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) =>{
-    res.status(200).send("Hello.")
-})
+app.get('/', (req, res) => {
+  res.status(200).send('Hello.');
+});
 
 export default app;
